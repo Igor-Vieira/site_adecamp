a884657f349e9bec4cf7e165337dc973e57e0d93
Igor-Vieira <igor30112000@gmail.com>
2020-09-11 15:27:42 -0300

    responsive layout

--- src/animations.js
+++ src/animations.js
@@ -4,6 +4,7 @@ window.onscroll = function () {
 
 const header = document.getElementById('myHeader')
 const button = document.getElementById('myButton')
+const icon = document.getElementById('myIcon')
 
 const topo = header.offsetTop
 
@@ -11,8 +12,10 @@ function animate() {
     if(window.pageYOffset > topo) {
         header.classList.add("stickyheader")
         button.classList.add("stickybutton")
+        icon.classList.add("stickyicon")
     } else {
         header.classList.remove("stickyheader")
         button.classList.remove("stickybutton")
+        icon.classList.remove("stickyicon")
     }
 }
